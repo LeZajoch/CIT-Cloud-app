e1c58a1504e3631fbc35d532cdb260293b17e4aa
LeZajoch <zajac@spsejecna.cz>
2024-10-07 22:47:56 +0200

    fix frontend

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.get("/notes",async(req, res)=> {
     const notes = await getNotes()
     res.send(notes)
 })
-//app.listen(8080, () => {
-  //  console.log('server is running on 8080')
-//})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
