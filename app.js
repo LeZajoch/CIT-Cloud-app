886f2f212ab5f84e72af3c1f6d4773108ca56764
LeZajoch <zajac@spsejecna.cz>
2024-10-07 22:12:36 +0200

    tutorial 3

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { getNotes } from './database.js';
+import cors from 'cors';
+
+
+ const app = express()
+
+ app.use(cors());
+
+app.get("/notes",async(req, res)=> {
+
+    const notes = await getNotes()
+    res.send(notes)
+})
+//app.listen(8080, () => {
+  //  console.log('server is running on 8080')
+//})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'your_user'
-    MYSQL_PASSWORD = 'your_password'
-    MYSQL_DB = 'blog_db'
-    MYSQL_CURSORCLASS = 'DictCursor'
